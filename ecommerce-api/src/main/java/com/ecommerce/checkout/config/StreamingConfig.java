6cee83d964ca094e31db57859dc55c42818db617
jmalcher <81192314+jmalcher@users.noreply.github.com>
2021-04-18 13:25:27 -0300

    Solução ecommerce com microserviços
    Solução ecommerce com microserviços

---
+++ ecommerce-api/src/main/java/com/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,14 @@
+package com.ecommerce.checkout.config;
+
+import com.ecommerce.checkout.streaming.PaymentPaidSink;
+import com.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
